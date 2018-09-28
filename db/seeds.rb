a72de1873f48420988d97784670eb8f6e64c5c58
Neenus Gabriel <neenus@live.ca>
2018-09-28 01:48:15 +0000

    seeded data for service providers with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# service_provider seed
+puts "Seeding Data ....."
+
+ServiceProvider.destroy_all
+
+sp1 = ServiceProvider.create(
+  first_name: Faker::Name.name,
+  last_name: Faker::Name.name,
+  email: Faker::Internet.free_email
+)
+
+sp2 = ServiceProvider.create(
+  first_name: Faker::Name.name,
+  last_name: Faker::Name.name,
+  email: Faker::Internet.free_email
+)
+
+sp3 = ServiceProvider.create(
+  first_name: Faker::Name.name,
+  last_name: Faker::Name.name,
+  email: Faker::Internet.free_email
+)
+puts "Seeding completed ....."
