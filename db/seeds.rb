694c860a87b5f7b308cedc698207f3c80a62045d
Thomassky28 <haifeng3@ualberta.ca>
2018-09-29 20:59:20 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,17 +81,17 @@ service2 = Service.create(
 puts "Seeding booking"
 
 
-booking1 = Booking.create(
-  customer_id: 1,
-  service_id: 1
-)
-booking2 = Booking.create(
-  customer_id: 1,
-  service_id: 2
-)
-booking3 = Booking.create(
-  customer_id: 1,
-  service_id: 3
-)
+# booking1 = Booking.create(
+#   customer_id: 1,
+#   service_id: 1
+# )
+# booking2 = Booking.create(
+#   customer_id: 1,
+#   service_id: 2
+# )
+# booking3 = Booking.create(
+#   customer_id: 1,
+#   service_id: 3
+# )
 
 puts "Seeding completed ....."
