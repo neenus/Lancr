fc5715660cbd6b0f91cf7a521fd6f93dbe434272
WilliamKasurak <willkdesign@gmail.com>
2018-09-29 21:24:40 +0000

    basic stripe dummy created

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   end
   resources :service_providers
 
+  resources :charges
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
