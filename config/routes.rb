7008cbff9f0adf30d7395424caf0abfd15de5856
WilliamKasurak <willkdesign@gmail.com>
2018-10-04 03:33:53 +0000

    final ui almost complete

--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,7 @@ Rails.application.routes.draw do
   post '/service_providers' => 'service_providers#create'
 
 
+
+  get "*missing" => "tests#404"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
