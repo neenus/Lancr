719a30ec439cd77abfaa1106eab763d68d7c6dc0
Vitor Oliveira <vbrazo@gmail.com>
2018-11-13 18:27:17 -0200

    Delete test folder

--- test/controllers/customer_sessions_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CustomerSessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/service_provider_sessions_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ServiceProviderSessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
