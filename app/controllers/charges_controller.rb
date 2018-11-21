68c57f1e2efbf5a9382d5aa5f12bcd0ac6d6519a
Vitor Oliveira <vbrazo@gmail.com>
2018-11-21 12:35:38 -0200

    Add missing controller specs

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,21 +3,14 @@ class ChargesController < ApplicationController
 def new
 end
 
-
-
 def create
   # Amount in cents
-
-
   @amount = Service.find(params[:service_id]).price * 100
-
-
 
   customer = Stripe::Customer.create(
     :email => current_customer.email,
     :source  => params[:stripeToken]
   )
-
 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
@@ -37,22 +30,13 @@ def create
         service.is_booked = true
         service.save
         redirect_to "/customers/#{current_customer.id} "
-
-
       else
         redirect_to '/services/params[service_id]'
       end
-
-
-
-
     end
-
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
     redirect_to '/customers/params[:service_id]'
   end
-
-
 end
